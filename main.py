4235460ca4990acd48150221eda2681cbed14b54
Geart Ferhati <55233184+geartprogrammer@users.noreply.github.com>
2020-04-12 18:35:18 +0200

    main.py
    Added the code

---
+++ main.py
@@ -0,0 +1,21 @@
+at = 50
+bt = 70
+ct = 100
+d = at + bt + ct
+
+ad = 30
+bd = 63
+cd = 70
+dd = ad + bd + cd
+
+if ad > bd and ad > cd:
+  print(ad)
+
+if bd > ad and bd > cd :
+  print(bb)
+
+if cd > ad and cd > bd:
+  print(cd)
+
+
+print(dd / 220)
